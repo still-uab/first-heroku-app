20ef6d59ea93dc406d5fe29b64e0dd7a6b99640b
still <still@uab.edu>
2021-01-20 17:55:22 -0600

    reverted changes in index files

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.set('port', (process.env.PORT || 5000 ));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
